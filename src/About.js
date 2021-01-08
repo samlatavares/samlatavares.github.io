3a63089cfe2f553a46cd2cc83af7da417c411305
Samla Azevedo Tavares <samla_azevedo@outlook.com>
2021-01-07 21:35:15 -0300

    Adjusting some phrases

--- src/About.js
+++ src/About.js
@@ -11,7 +11,7 @@ export default function About() {
       <p><i class="fas fa-user-edit"></i> I'm always improving soft and hard skills.</p>
       <p><i class="fas fa-laptop"></i> My main stack is C#/.Net but I'm currently working with Java.</p>
       <p><i class="fas fa-binoculars"></i> If you want to know what I'm studying now you can check in my Github or text me (please check the <a href="#contactDiv">contact session</a> ).</p>
-      <p><i class="fas fa-globe"></i> My native language it's Portuguese, I'm an English intermediate speaker, Spanish beginner speaker and I'm studying Germany by myself.</p>
+      <p><i class="fas fa-globe"></i> My native language it's Portuguese, I'm an English intermediary speaker, Spanish beginner speaker and I'm studying German by myself.</p>
       <p><i class="fas fa-gamepad"></i> Some hobbies: Watch movies and TV series, eat, cooking, read, learn languages, listen to music, learn to draw, and sometimes play games.</p>
     </div>
   </div>;
